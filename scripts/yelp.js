c7b7be080d576c80408162f7178d29ca36b7d97e
Brett Tomita <bretttomita@gmail.com>
2019-03-25 13:35:25 -0700

    yelp.js update

--- scripts/yelp.js
+++ scripts/yelp.js
@@ -1,31 +1,45 @@
 'use strict';
-define(function (require) {
-    const yelp = require('yelp-fusion');
-});
-//document.getElementById("container").innerHTML = "test";
+
+
+function printSearch(e)
+{
+	var search = document.getElementById("input").value;
+	document.getElementById("container").innerHTML = search;
+	return false;
+}
+
+if(search.attachEvent)
+{
+	search.attachEvent("submit", printSearch);
+}
+else
+{
+	search.addEventListener("submit", printSearch);
+}
+const yelp = require('yelp-fusion');
+
 // Place holder for Yelp Fusion's API Key. Grab them
 // from https://www.yelp.com/developers/v3/manage_app
 const apiKey = 'p8eXXM3q_ks6WY_FWc2KhV-EmLhSpbJf0P-SATBhAIM4dNCgsp3sH8ogzJPezOT6LzFQlb_vcFfxziHbHuNt8RwxtWY0-vRpx7C0nPz5apIT4A5LYGmaVfuwPrf3WXYx';
 
-var filter = "mexican";
-var place = "Huntington Beach, CA";
-const searchRequest = {
-  // term: "Mexican",
-  // location: 'Huntington Beach, CA'
-  filter, place
-};
-
-const yelpClient = yelp.client(apiKey);
-
-yelpClient.search(searchRequest).then(response => 
+function yelpSearch(e, request)
 {
-	var i;
-	for(i in response.jsonBody.businesses)
-	{
-	  const result = response.jsonBody.businesses[i];
-	  const prettyJson = JSON.stringify(result, null, 4);
-	  console.log(prettyJson);
-	}
-}).catch(e => {
-  console.log(e);
-});
+	const searchRequest = {
+	  term: request,
+	  location: 'huntington beach, ca'
+	};
+
+	const client = yelp.client(apiKey);
+
+	client.search(searchRequest).then(response => {
+		var i;
+		for(i in response.jsonBody.businesses)
+		{
+			const result = response.jsonBody.businesses[i];
+			const jsonResult = JSON.stringify(result, null, 4);
+			console.log(jsonResult);
+		}
+	}).catch(e => {
+	  console.log(e);
+	});
+}
